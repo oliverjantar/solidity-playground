034eba87af569dba93e4a9fa01c660c14b450617
jiriandras <jiriandras@protonmail.com>
2022-06-27 15:50:43 +0200

    Update ERC1155 check signature

--- nft/contracts/MySplitNft.sol
+++ nft/contracts/MySplitNft.sol
@@ -2,20 +2,49 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "./MyNft.sol";
-
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract MySplitNft is ERC1155 {
+    bytes[] private signatures;
+
     constructor(string memory uri) ERC1155(uri) {}
 
-    function lock(MyNft nft, uint256 tokenId) public{
-        require(nft.getApproved(tokenId) == address(this),"Not approved to transfer NFT");
+    function lock(
+        MyNft nft,
+        uint256 tokenId,
+        bytes memory signature
+    ) public {
+        require(
+            nft.getApproved(tokenId) == address(this),
+            "Not approved to transfer NFT"
+        );
         nft.transferFrom(msg.sender, address(this), tokenId);
-        _mint(msg.sender, tokenId, 5,"");
+
+        signatures.push(signature);
+
+        address x = ECDSA.recover(message, signature);
+        require(x == msg.sender);
+
+        //  _mint(msg.sender, tokenId, 5,"");
+
+        //emitnu zpravu o vygenerovany zprave, predam ji nekomu jinymu offchain a v unlocku ji zkontroluju
     }
 
-    function unlock(MyNft nft, uint256 tokenId) public{
-        require(balanceOf(msg.sender, tokenId) == 5,"Insufficient balance of NFTs");
-        nft.safeTransferFrom(address(this),msg.sender, tokenId);
-        _burn(msg.sender, tokenId, 5);
+    function unlock(
+        MyNft nft,
+        uint256 tokenId,
+        bytes memory signature,
+        string memory message
+    ) public {
+        require(
+            balanceOf(msg.sender, tokenId) == 5,
+            "Insufficient balance of NFTs"
+        );
+        nft.safeTransferFrom(address(this), msg.sender, tokenId);
+
+        address x = ECDSA.recover(message, signature);
+        require(x == msg.sender);
+
+        //  _burn(msg.sender, tokenId, 5);
     }
 }
