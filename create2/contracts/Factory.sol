3dc8d7a416922766835692219911def03f8da201
jiriandras <jiriandras@protonmail.com>
2022-08-18 15:14:03 +0200

    More examples with assembly

--- create2/contracts/Factory.sol
+++ create2/contracts/Factory.sol
@@ -54,4 +54,93 @@ contract Item {
             sstore(0x0, n)
         }
     }
+
+    function setNumberArray(uint32[] calldata number) public {
+        bytes32 foo;
+        bytes32 foo2;
+        bytes32 foo3;
+        assembly {
+            foo := calldataload(4) // na 32bytu je pointer na slot delky
+            foo2 := calldataload(36) // delka pole
+            foo3 := calldataload(68) // konkretni 1. cislo v poli
+        }
+        console.logBytes(abi.encode(foo));
+        console.logBytes(abi.encode(foo2));
+        console.logBytes(abi.encode(foo3));
+    }
+
+    function setNumberArray2(uint32[] calldata a, uint32[] calldata b) public {
+        bytes32 a1;
+        bytes32 a2;
+        bytes32 a3;
+
+        bytes32 b1;
+        bytes32 b2;
+        bytes32 b3;
+
+        assembly {
+            a1 := calldataload(4) // na 32 bytu je pointer na slot delky
+            a2 := calldataload(68) // delka pole
+            a3 := calldataload(100) // konkretni 1. cislo v poli
+
+            b1 := calldataload(36)
+            b2 := calldataload(132) // 128 + 4 (selector funkce)
+            b3 := calldataload(164)
+        }
+        console.logBytes(abi.encode(a1));
+        console.logBytes(abi.encode(a2));
+        console.logBytes(abi.encode(a3));
+
+        console.logBytes(abi.encode(b1));
+        console.logBytes(abi.encode(b2));
+        console.logBytes(abi.encode(b3));
+    }
+}
+
+contract FakeNumber {
+    function getNumber() public pure returns (uint32) {
+        return 456;
+    }
+
+    constructor(bytes memory originalBytecode) {
+        assembly {
+            return(0xc0, 411) // 192, 192 + 219
+        }
+    }
+}
+
+contract OriginalNumber {
+    function getNumber() public pure returns (uint32) {
+        return 123;
+    }
+}
+
+contract ContractA {
+    constructor(bytes memory a) public {
+        /*
+           The Values to return opcode can come from a complex logic 
+           but to keep it simple and for demo, we have hard coded them. 
+           It may be different in your case. 
+        */
+
+        assembly {
+            return(0xc0, add(0xc0, 414))
+        }
+    }
+
+    function sing() public pure returns (string memory) {
+        return "It's a Wonderful World!";
+    }
+}
+
+contract AlteredContractA {
+    function sing() public pure returns (string memory) {
+        return "The Times They Are a Changin!";
+    }
+}
+
+contract AlteredContractB {
+    function sing() public pure returns (string memory) {
+        return "Lorem ipsum";
+    }
 }
